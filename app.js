d645793af42cb5fe538c860f1b40ccbda80c747f
yfsoftcom <yfsoftcom@126.com>
2018-06-25 10:05:47 +0800

    init

---
+++ app.js
@@ -0,0 +1,42 @@
+const Fpm = require('yf-fpm-server').Fpm
+const path = require('path')
+
+const fpm = new Fpm()
+
+const biz = fpm.createBiz('0.0.1')
+
+biz.addSubModules('foo', {
+    bar: args => {
+        return Promise.resolve(1)
+    }
+})
+fpm.addBizModules(biz)
+
+const scriptPath = path.join(__dirname, 'shell', 'codepull.sh')
+/**
+ * /webhook/:upstream/:type/:data
+ * upstream: 来源, weixin,fir.im...
+ * type: notify, message, hook
+ * data: 数据
+ * curl -H "Content-Type:application/json" -X POST --data '{"id":10}' http://localhost:9003/webhook/codepull/p/api.yunplus.io
+ */
+
+// add webhook subscribe
+// 
+const generate = (type) =>{
+    return async (topic, message) => {
+        const project = message.url_data
+        try {
+            let result = await fpm.execShell(scriptPath, ['pull', '-' + type, project])
+            fpm.logger.info(result)
+        }catch(e){
+            fpm.logger.error(e)
+        }
+    }
+}
+fpm.subscribe('#webhook/codepull/p', generate('p'))
+fpm.subscribe('#webhook/codepull/w', generate('w'))
+
+fpm.run().then( () => {
+    fpm.logger.info('Ready To GO~')
+})
